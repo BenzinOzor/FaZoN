861da18fe7ffc7823b6c6ba09a50d50cdd5f2e8f
Benzine <fryfonbly@gmail.com>
2025-04-15 18:40:27 +0200

    Add std::variant wrapper class

--- FrameWork/FrameWork/Code/FZN/DataStructure/Variant.h
+++ FrameWork/FrameWork/Code/FZN/DataStructure/Variant.h
@@ -16,31 +16,31 @@ namespace fzn
 	{
 	public:
 		Variant() = default;
-		/*template <class _Ty>
-		Variant( const _Ty& _obj ) : m_value( _obj ) {}
-		template <class _Ty>
-		Variant( _Ty& _obj ) : m_value( _obj ) {}
-		template <class _Ty>
-		Variant( const _Ty&& _obj ) : m_value( _obj ) {}*/
-		template <class _Ty>
-		Variant( _Ty&& _obj ) : m_value( _obj ) {}
 		Variant( const Variant& _variant ): m_value( _variant.m_value ) {}
-		Variant( Variant&& _variant ) { std::swap( m_value, _variant.m_value ); }
+		Variant( Variant&& _variant ) noexcept { std::swap( m_value, _variant.m_value ); }
+		template <class _Ty,
+			std::enable_if_t< sizeof...( Types ) != 0
+			&& std::is_same_v< std::remove_cvref_t<_Ty>, Variant > == false,
+			int> = 0 >
+		Variant( _Ty&& _obj ) : m_value( _obj ) {}
+
+		template <class _Ty,
+			std::enable_if_t< sizeof...( Types ) != 0
+			&& std::is_same_v< std::remove_cvref_t<_Ty>, Variant > == false,
+			int> = 0 >
+		Variant& operator=( _Ty&& _Obj )						{ return m_value = _Obj; }
+		Variant& operator=( const Variant& _variant )			{ m_value = _variant.m_value; return *this; }
+		Variant& operator=( Variant& _variant )					{ m_value = _variant.m_value; return *this; }
+		Variant& operator=( const Variant&& _variant ) noexcept	{ std::swap( m_value, _variant.m_value ); return *this; }
+		Variant& operator=( Variant&& _variant )  noexcept		{ std::swap( m_value, _variant.m_value ); return *this; }
+
+		bool operator==( const Variant& _variant ) const { return m_value == _variant.m_value; }
 
 		template < typename Type >
 		Type get()
 		{
 			return std::get< Type >( m_value );
 		}
-
-		template <class _Ty>
-		Variant& operator=( _Ty&& _Obj )				{ return m_value.operator =( _Obj ); }
-		Variant& operator=( const Variant& _variant )	{ m_value = _variant.m_value; return *this; }
-		Variant& operator=( Variant& _variant )			{ m_value = _variant.m_value; return *this; }
-		Variant& operator=( const Variant&& _variant )	{ std::swap( m_value, _variant.m_value ); return *this; }
-		Variant& operator=( Variant&& _variant )		{ std::swap( m_value, _variant.m_value ); return *this; }
-
-		bool operator==( const Variant& _variant ) const { return m_value == _variant.m_value; }
 
 		template < typename Type >
 		const Type get() const
@@ -66,11 +66,25 @@ namespace fzn
 			return std::holds_alternative< Type >( m_value );
 		}
 
-		template< typename Ret, class Visitor, Types... >
-		const Ret visit( Visitor&& _visitor ) const { return std::visit( _visitor, m_value ) ; }
+		template< typename Ret, class Visitor >
+		const Ret visit( Visitor&& _visitor ) const
+		{
+			std::variant< Types... > temp_variant = m_value;
+			return std::visit< Ret >( _visitor, temp_variant );
+		}
 
-		template< typename Ret, class Visitor, Types... >
-		Ret visit( Visitor&& _visitor ) { return std::visit( _visitor, m_value ); }
+		template< typename Ret, class Visitor >
+		Ret visit( Visitor&& _visitor ) { return std::visit< Ret >( _visitor, m_value ); }
+
+		template< class Visitor >
+		const void visit( Visitor&& _visitor ) const
+		{
+			std::variant< Types... > temp_variant = m_value;
+			std::visit( _visitor, temp_variant );
+		}
+
+		template< class Visitor >
+		void visit( Visitor&& _visitor ) { std::visit( _visitor, m_value ); }
 
 		std::variant< Types... >& get_value() { return m_value; }
 		const std::variant< Types... >& get_value() const { return m_value; }
