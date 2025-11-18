c03e026bb361693c3d44118f291b981095e71035
Benzine <fryfonbly@gmail.com>
2025-11-18 08:39:23 +0100

    Add chrono class

---
+++ FrameWork/FrameWork/Code/FZN/Tools/Chrono.cpp
@@ -0,0 +1,52 @@
+#include "Chrono.h"
+
+namespace fzn
+{
+	Chrono::Chrono()
+	{
+		m_paused = true;
+		m_previous_clock = std::chrono::steady_clock::now();
+		m_chrono = std::chrono::milliseconds( 0 );
+	}
+
+	void Chrono::start()
+	{
+		m_paused = false;
+		m_chrono = std::chrono::milliseconds( 0 );
+		m_previous_clock = std::chrono::steady_clock::now();
+	}
+
+	void Chrono::toggle_pause()
+	{
+		m_paused = !m_paused;
+
+		if( m_paused == false )
+			m_previous_clock = std::chrono::steady_clock::now();
+	}
+
+	void Chrono::stop()
+	{
+		m_paused = true;
+		m_chrono = std::chrono::milliseconds( 0 );
+	}
+
+	bool Chrono::is_paused()
+	{
+		return m_paused;
+	}
+
+	void Chrono::update()
+	{
+		if( m_paused )
+			return;
+
+		const std::chrono::steady_clock::time_point current_clock = std::chrono::steady_clock::now();
+
+		std::chrono::duration< int, std::milli > delta = std::chrono::duration_cast<std::chrono::milliseconds>( current_clock - m_previous_clock );
+
+		m_chrono += delta;
+
+		m_previous_clock = std::chrono::steady_clock::now();
+	}
+
+}
---
+++ FrameWork/FrameWork/Code/FZN/Tools/Chrono.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <chrono>
+
+#include "FZN/Defines.h"
+
+
+namespace fzn
+{
+	class FZN_EXPORT Chrono
+	{
+	public:
+		Chrono();
+
+		void start();
+		void toggle_pause();
+		void stop();
+
+		bool is_paused();
+
+		void update();
+		const std::chrono::duration< int, std::milli >& get_time() { return m_chrono; }
+
+	private:
+		std::chrono::duration< int, std::milli > m_chrono;
+
+		std::chrono::steady_clock::time_point m_previous_clock;
+		bool m_paused{ true };
+	};
+};
