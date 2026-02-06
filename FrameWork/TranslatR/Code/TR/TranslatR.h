a18ce51b52752a1a48001f670d4f5be9c2c0a4aa
Benzine <fryfonbly@gmail.com>
2026-02-06 12:45:22 +0100

    [TR] Add comments to TranslatR functions

--- FrameWork/TranslatR/Code/TR/TranslatR.h
+++ FrameWork/TranslatR/Code/TR/TranslatR.h
@@ -43,21 +43,32 @@ namespace TR
 		**/
 		void _display_new_entry( uint32_t _row_id );
 
+		/**
+		* @brief Enable the new language popup and clear new language strings.
+		**/
 		void _show_new_language_popup();
+		/**
+		* @brief Display and manage the new language popup.
+		**/
 		void _add_language_popup();
+		/**
+		* @brief Validate the new languages entered in the popup and add them to the projects available languages.
+		**/
 		void _add_new_languages();
+		/**
+		* @brief Remove the given language from the projects available ones and from all the entries.
+		* @param _language_id The ID of the language in the languages vector.
+		**/
 		void _remove_language( uint32_t _language_id );
-		
-		void _create_json();
 
 	private:
-		fzn::Localisation::LocalisationData	m_loc_data;
+		fzn::Localisation::LocalisationData	m_loc_data;			// Current project localisation data.
 
 		FileManager		m_file_manager;
 
-		std::string		m_new_entry{};
-		StringVector	m_new_languages{};
-		std::string		m_new_language{};
-		bool			m_show_new_language_popup{ false };
+		std::string		m_new_entry{};							// The empty entry displayed after the ones in the data. It will be added in the entries list once validated and cleared to allow for the next addition.
+		StringVector	m_new_languages{};						// The languages added by the popup.
+		std::string		m_new_language{};						// The language to add in the languages list of the popup.
+		bool			m_show_new_language_popup{ false }; 	// True if we are currently displaying the language addition popup.
 	};
 }
